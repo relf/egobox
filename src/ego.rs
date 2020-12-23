08bbe427ff51effec632529e6e3124d4b3b06fcd
relf <remi.lafage@onera.fr>
2020-12-23 23:10:55 +0100

    fix bug in ei

--- src/ego.rs
+++ src/ego.rs
@@ -4,6 +4,7 @@ use crate::gaussian_process::{ConstantMean, GaussianProcess, SquaredExponentialK
 use finitediff::FiniteDiff;
 use libm::erfc;
 use ndarray::{s, stack, Array, Array1, Array2, ArrayBase, ArrayView, Axis, Data, Ix2, Zip};
+use ndarray_npy::write_npy;
 use ndarray_rand::rand::{Rng, SeedableRng};
 use ndarray_stats::QuantileExt;
 use nlopt::*;
@@ -29,15 +30,15 @@ pub enum QEiStrategy {
 }
 
 pub struct Ego<F: ObjFn, R: Rng> {
-    n_iter: usize,
-    n_start: usize,
-    n_parallel: usize,
-    n_doe: usize,
-    x_doe: Option<Array2<f64>>,
-    xlimits: Array2<f64>,
-    q_ei: QEiStrategy,
-    obj: F,
-    rng: R,
+    pub n_iter: usize,
+    pub n_start: usize,
+    pub n_parallel: usize,
+    pub n_doe: usize,
+    pub x_doe: Option<Array2<f64>>,
+    pub xlimits: Array2<f64>,
+    pub q_ei: QEiStrategy,
+    pub obj: F,
+    pub rng: R,
 }
 
 impl<F: ObjFn> Ego<F, Isaac64Rng> {
@@ -113,8 +114,7 @@ impl<F: ObjFn, R: Rng + Clone> Ego<F, R> {
 
         let mut y_data = self.obj_eval(&x_data);
 
-        for k in 0..self.n_iter {
-            println!("k={}", k);
+        for _ in 0..self.n_iter {
             for _ in 0..self.n_parallel {
                 let gpr = GaussianProcess::<ConstantMean, SquaredExponentialKernel>::params(
                     ConstantMean::default(),
@@ -123,23 +123,32 @@ impl<F: ObjFn, R: Rng + Clone> Ego<F, R> {
                 .fit(&x_data, &y_data)
                 .expect("GP training failure");
 
+                let f_min = y_data.min().unwrap();
+                if false {
+                    let xplot = Array::linspace(0., 25., 100).insert_axis(Axis(1));
+                    let obj = self.obj_eval(&xplot);
+                    let gpr_vals = gpr.predict_values(&xplot).unwrap();
+                    let gpr_vars = gpr.predict_variances(&xplot).unwrap();
+                    let ei = xplot.map(|x| Self::ei(&[*x], &gpr, *f_min));
+                    println!("ei={:?}", ei);
+                    write_npy("ego_x.npy", xplot).expect("xplot saved");
+                    write_npy("ego_obj.npy", obj).expect("obj saved");
+                    write_npy("ego_gpr.npy", gpr_vals).expect("gp vals saved");
+                    write_npy("ego_gpr_vars.npy", gpr_vars).expect("gp vars saved");
+                    write_npy("ego_ei.npy", ei).expect("gp vars saved");
+                }
+
                 match self.find_best_point(&x_data, &y_data, &sampling, &gpr) {
-                    Ok(xk) => {
-                        println!("xk={:?}", xk);
-                        match self.get_virtual_point(&xk, &y_data, &gpr) {
-                            Ok(yk) => {
-                                println!("yk={:?}", yk);
-                                y_data = stack![Axis(0), y_data, Array2::from_elem((1, 1), yk)];
-                                x_data = stack![Axis(0), x_data, xk.insert_axis(Axis(0))];
-                            }
-                            Err(err) => {
-                                println!("ERROR={:?}", err);
-                                break;
-                            }
+                    Ok(xk) => match self.get_virtual_point(&xk, &y_data, &gpr) {
+                        Ok(yk) => {
+                            y_data = stack![Axis(0), y_data, Array2::from_elem((1, 1), yk)];
+                            x_data = stack![Axis(0), x_data, xk.insert_axis(Axis(0))];
                         }
-                    }
-                    Err(err) => {
-                        println!("ERROR={:?}", err);
+                        Err(_) => {
+                            break;
+                        }
+                    },
+                    Err(_) => {
                         break;
                     }
                 }
@@ -153,9 +162,6 @@ impl<F: ObjFn, R: Rng + Clone> Ego<F, R> {
         }
 
         let best_index = y_data.argmin().unwrap().0;
-        println!("xdata = {:?}", x_data);
-        println!("ydata = {:?}", y_data);
-        println!("true = {:?}", self.obj_eval(&x_data));
         OptimResult {
             x_opt: x_data.row(best_index).to_owned(),
             y_opt: y_data.row(best_index)[0],
@@ -204,9 +210,7 @@ impl<F: ObjFn, R: Rng + Clone> Ego<F, R> {
                             success = true;
                         }
                     }
-                    Err((_, err)) => {
-                        println!("opterr = {:?}", err);
-                    }
+                    Err((_, _)) => {}
                 }
             }
             n_optim += 1;
@@ -237,7 +241,7 @@ impl<F: ObjFn, R: Rng + Clone> Ego<F, R> {
             if let Ok(s) = gpr.predict_variances(&pt) {
                 let pred = p[[0, 0]];
                 let sigma = f64::sqrt(s[[0, 0]]);
-                let args0 = f_min - pred / sigma;
+                let args0 = (f_min - pred) / sigma;
                 let args1 = (f_min - pred) * Self::norm_cdf(args0);
                 let args2 = sigma * Self::norm_pdf(args0);
                 args1 + args2
@@ -288,7 +292,7 @@ mod tests {
         println!("xsinx optim result = {:?}", res);
         println!("Elapsed = {:?}", now.elapsed());
         let expected = array![18.9];
-        assert_abs_diff_eq!(expected, res.x_opt, epsilon = 1e-1);
+        assert_abs_diff_eq!(expected, res.x_opt, epsilon = 5e-1);
     }
 
     #[test]
@@ -303,6 +307,6 @@ mod tests {
         println!("Rosenbrock optim result = {:?}", res);
         println!("Elapsed = {:?}", now.elapsed());
         let expected = array![1., 1.];
-        assert_abs_diff_eq!(expected, res.x_opt, epsilon = 1e-1);
+        assert_abs_diff_eq!(expected, res.x_opt, epsilon = 5e-1);
     }
 }
