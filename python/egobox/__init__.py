297e7b61be5f9a34f0945e339df7120965351a61
relf <remi.lafage@onera.fr>
2021-10-14 10:57:54 +0200

    Initiate python binding

---
+++ python/egobox/__init__.py
@@ -0,0 +1,1 @@
+from .egobox import *
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,26 @@
 pub mod ego;
 pub mod errors;
+
+use doe::{SamplingMethod, LHS};
+use ndarray::{arr2, Array2, Ix2};
+use numpy::{IntoPyArray, PyArray, PyReadonlyArray};
+use pyo3::prelude::*;
+
+/// Formats the sum of two numbers as string.
+#[pyfunction]
+fn lhs<'py>(
+    py: Python<'py>,
+    xlimits: PyReadonlyArray<f64, Ix2>,
+    a: usize,
+) -> &'py PyArray<f64, Ix2> {
+    let actual = LHS::new(&xlimits.as_array()).sample(a);
+
+    actual.into_pyarray(py)
+}
+
+#[pymodule]
+fn egobox(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(lhs, m)?)?;
+
+    Ok(())
+}
