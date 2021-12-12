9b0f3ed22d3bdd5e2a4470ce6b80ef4a48c0f628
relf <remi.lafage@onera.fr>
2021-12-12 22:12:50 +0100

    Add segomoe test case: hesse

--- python/egobox/tests/test_segomoe_cases.py
+++ python/egobox/tests/test_segomoe_cases.py
@@ -4,26 +4,42 @@ import egobox as egx
 import logging
 from segomoe.cases.case_generator import _import_case
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 
-def test_egor(case, options={}):
-    options = {}
-    case = _import_case("Mod_Branin", options)()
+def create_egor(case, **options):
+    opts = {}
+    case = _import_case(case, opts)()
     xlimits = np.array([[v["lb"], v["ub"]] for v in case["vars"]])
     n_cstr = len(case["con"])
     fun = case["f_grouped"]
     f_grouped = lambda x: np.atleast_2d(
         np.array([fun(xi)[0] for xi in np.atleast_2d(x)])
     )
-    return egx.Optimizer(fun=f_grouped, xlimits=xlimits, n_cstr=n_cstr), case["sol"]
+    return (
+        egx.Optimizer(fun=f_grouped, xlimits=xlimits, n_cstr=n_cstr, **options),
+        case["sol"],
+    )
 
 
 class TestEgorOnSegomoeCases(unittest.TestCase):
     def test_branin(self):
-        egor, expected = test_egor("Mod_Branin")
+        options = {"seed": 42, "n_doe": 8}
+        egor, expected = create_egor("Mod_Branin", **options)
+        res = egor.minimize(n_eval=50)
+        self.assertAlmostEqual(expected["value"], res.y_opt[0], delta=expected["tol"])
+
+    def test_hesse(self):
+        options = {
+            "seed": 42,
+            "kpls_dim": 3,
+            "n_doe": 8,
+            "regr_spec": 1,
+            "corr_spec": 1,
+        }
+        egor, expected = create_egor("Hesse", **options)
         res = egor.minimize(n_eval=30)
-        self.assertAlmostEqual(expected["value"], res.y_opt[0], delta=1e-2)
+        self.assertAlmostEqual(expected["value"], res.y_opt[0], delta=expected["tol"])
 
 
 if __name__ == "__main__":
