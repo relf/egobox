b5c8c68e52d00eb6961deab798ea609cba53d703
relf <remi.lafage@onera.fr>
2023-06-02 22:18:22 +0200

    Re-export moe spec enums

--- ego/src/types.rs
+++ ego/src/types.rs
@@ -1,10 +1,12 @@
 use crate::errors::Result;
 use argmin::core::CostFunction;
 use egobox_moe::{ClusteredSurrogate, Clustering};
-pub use egobox_moe::{CorrelationSpec, RegressionSpec};
 use linfa::Float;
 use ndarray::{Array1, Array2, ArrayView2};
 use serde::{Deserialize, Serialize};
+
+/// Enums for regression and correlation selection
+pub use egobox_moe::{CorrelationSpec, RegressionSpec};
 
 /// Optimization result
 #[derive(Clone, Debug)]
