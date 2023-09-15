e2bf1c64ddbc1c4c7782d046a31488829d5513d6
relf <remi.lafage@onera.fr>
2023-09-15 14:31:23 +0200

    Fix py test

--- python/egobox/tests/test_mixintegor.py
+++ python/egobox/tests/test_mixintegor.py
@@ -30,11 +30,11 @@ def mixobj(X):
     i = X[:, 3]
 
     y = (x2 + 2 * x3 + 3 * x4) * x5 * x1 + (x2 + 2 * x3 + 3 * x4) * x6 * 0.95 * x1 + i
-    return y
+    return y.reshape(-1, 1)
 
 
 class TestMixintEgx(unittest.TestCase):
-    def test_xsinx(self):
+    def test_int(self):
         xtypes = [egx.XSpec(egx.XType(egx.XType.INT), [0.0, 25.0])]
 
         egor = egx.Egor(xsinx, xtypes, seed=42, n_doe=3)
@@ -43,21 +43,20 @@ class TestMixintEgx(unittest.TestCase):
         self.assertAlmostEqual(-15.125, res.y_opt[0], delta=5e-3)
         self.assertAlmostEqual(19, res.x_opt[0], delta=1)
 
-    def test_ego_mixed_integer(self):
-        n_iter = 15
-        n_doe = 5
-        random_state = 42
+    def test_ord_enum(self):
         xtypes = [
             egx.XSpec(egx.XType(egx.XType.FLOAT), [-5.0, 5.0]),
-            egx.XSpec(egx.XType(egx.XType.ENUM), xnames=["blue", "red", "green"]),
-            egx.XSpec(egx.XType(egx.XType.ENUM), xnames=["large", "small"]),
+            egx.XSpec(egx.XType(egx.XType.ENUM), tags=["blue", "red", "green"]),
+            egx.XSpec(egx.XType(egx.XType.ENUM), xlimits=[2]),
             egx.XSpec(egx.XType(egx.XType.ORD), [0, 2, 3]),
         ]
-        egor = egx.Egor(mixobj, xtypes, seed=42, n_doe=3)
+        egor = egx.Egor(mixobj, xtypes, seed=42)
         res = egor.minimize(n_iter=10)
-        print(f"Optimization f={res.y_opt} at {res.x_opt}")
-        self.assertAlmostEqual(-15.125, res.y_opt[0], delta=5e-3)
-        self.assertAlmostEqual(19, res.x_opt[0], delta=1)
+        self.assertAlmostEqual(-14.25, res.y_opt[0])
+        self.assertAlmostEqual(-5, res.x_opt[0])
+        self.assertAlmostEqual(2, res.x_opt[1])
+        self.assertAlmostEqual(1, res.x_opt[2])
+        self.assertAlmostEqual(0, res.x_opt[3])
 
 
 if __name__ == "__main__":
