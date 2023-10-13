6f1af3ad195cd84db541fc3b14a0218f1beb240c
relf <remi.lafage@onera.fr>
2023-10-13 17:16:59 +0200

    Remove useless Optimizer trait

--- ego/src/types.rs
+++ ego/src/types.rs
@@ -141,14 +141,11 @@ pub trait SurrogateBuilder: Clone + Serialize + Sync {
 pub trait ObjFn<U>: Fn(&[f64], Option<&mut [f64]>, &mut U) -> f64 {}
 impl<T, U> ObjFn<U> for T where T: Fn(&[f64], Option<&mut [f64]>, &mut U) -> f64 {}
 
+#[derive(Copy, Clone, Debug)]
 pub enum Algorithm {
     Cobyla,
     Slsqp,
     Lhs,
-}
-
-pub trait Optimizer {
-    fn minimize(&self) -> std::result::Result<(Array1<f64>, f64), ()>;
 }
 
 /// Data used by internal infill criteria to be optimized
