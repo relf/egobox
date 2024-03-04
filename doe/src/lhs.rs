2676ccca4cb785919ad8d7f43a4cac1431e03af9
relf <remi.lafage@onera.fr>
2024-03-04 17:15:18 +0100

    Fix LHS maximin

--- doe/src/lhs.rs
+++ doe/src/lhs.rs
@@ -268,21 +268,26 @@ impl<F: Float, R: Rng + Clone> Lhs<F, R> {
     }
 
     fn _maximin_lhs(&self, ns: usize, centered: bool, max_iters: usize) -> Array2<F> {
-        let mut max_dist = F::zero();
-        let mut lhs = self._classic_lhs(ns);
-        for _ in 0..max_iters {
+        let mut lhs = if centered {
+            self._centered_lhs(ns)
+        } else {
+            self._classic_lhs(ns)
+        };
+        let mut max_dist = *pdist(&lhs).min().unwrap();
+        let mut lhs_maximin = lhs;
+        for _ in 0..max_iters - 1 {
             if centered {
                 lhs = self._centered_lhs(ns);
             } else {
                 lhs = self._classic_lhs(ns);
             }
-            let d = pdist(&lhs);
-            let d_min = F::cast(*d.min().unwrap());
+            let d_min = *pdist(&lhs).min().unwrap();
             if max_dist < d_min {
-                max_dist = d_min
+                max_dist = d_min;
+                std::mem::swap(&mut lhs_maximin, &mut lhs)
             }
         }
-        lhs
+        lhs_maximin
     }
 }
 
