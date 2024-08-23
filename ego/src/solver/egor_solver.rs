e582b839e380bd975eb1be8c07d15ad725079974
relf <remi.lafage@onera.fr>
2024-08-23 15:55:37 +0200

    Improve TREGO logging

--- ego/src/solver/egor_solver.rs
+++ ego/src/solver/egor_solver.rs
@@ -326,14 +326,16 @@ where
         let prev_best = state.prev_best_index.unwrap(); // initialized in init
 
         // Check prev step success
+        let diff = y_data[[prev_best, 0]] - rho(state.sigma);
+        let last_iter_success = y_data[[best, 0]] < diff;
         info!(
-            "success = {} as {} < {} - {}",
-            y_data[[best, 0]] < (y_data[[prev_best, 0]] - rho(state.sigma)),
+            "success = {} as {} {} {} - {}",
+            last_iter_success,
             y_data[[best, 0]],
+            if last_iter_success { "<" } else { ">=" },
             y_data[[prev_best, 0]],
             rho(state.sigma)
         );
-        let last_iter_success = y_data[[best, 0]] < (y_data[[prev_best, 0]] - rho(state.sigma));
         let mut new_state = state.clone();
         if !state.prev_step_ego && state.get_iter() != 0 {
             // Adjust trust region wrt local step success
