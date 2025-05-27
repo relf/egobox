4f3a5c510708a75da221881d1a77fc8fae3a126e
relf <remi.lafage@onera.fr>
2025-05-27 10:06:42 +0200

    Adjust test max_iters

--- python/egobox/tests/test_egor.py
+++ python/egobox/tests/test_egor.py
@@ -141,7 +141,7 @@ class TestOptimizer(unittest.TestCase):
 
     def test_g24(self):
         n_doe = 5
-        max_iters = 20
+        max_iters = 30
         n_cstr = 2
         egor = egx.Egor(
             egx.to_specs([[0.0, 3.0], [0.0, 4.0]]),
