7a8dca291c1e90ef4392de0947d6602e15b775dd
relf <remi.lafage@onera.fr>
2025-06-16 10:59:37 +0200

    Cleanup

--- crates/ego/src/solver/solver_infill_optim.rs
+++ crates/ego/src/solver/solver_infill_optim.rs
@@ -10,7 +10,7 @@ use crate::types::ObjFn;
 use nlopt::ObjFn;
 
 use egobox_moe::MixtureGpSurrogate;
-use log::{debug, info};
+use log::info;
 use ndarray::{Array, Array1, Array2, Axis};
 
 use rayon::prelude::*;
