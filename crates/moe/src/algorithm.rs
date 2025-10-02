788b9a9a5576fa620b20c624f8b57157bce230d3
relf <remi.lafage@onera.fr>
2025-10-02 11:06:25 +0200

    Put typetag attr behind feature gate

--- crates/moe/src/algorithm.rs
+++ crates/moe/src/algorithm.rs
@@ -477,7 +477,7 @@ impl Clustered for GpMixture {
     }
 }
 
-#[typetag::serde]
+#[cfg_attr(feature = "serializable", typetag::serde)]
 impl GpSurrogate for GpMixture {
     fn dims(&self) -> (usize, usize) {
         self.experts[0].dims()
@@ -515,7 +515,7 @@ impl GpSurrogate for GpMixture {
     }
 }
 
-#[typetag::serde]
+#[cfg_attr(feature = "serializable", typetag::serde)]
 impl GpSurrogateExt for GpMixture {
     fn predict_gradients(&self, x: &ArrayView2<f64>) -> Result<Array2<f64>> {
         match self.recombination {
@@ -552,7 +552,7 @@ impl GpScore<MoeError, GpMixtureParams<f64>, Self> for GpMixture {
     }
 }
 
-#[typetag::serde]
+#[cfg_attr(feature = "serializable", typetag::serde)]
 impl GpQualityAssurance for GpMixture {
     fn training_data(&self) -> &(Array2<f64>, Array1<f64>) {
         (self as &dyn GpScore<_, _, _>).training_data()
@@ -573,7 +573,7 @@ impl GpQualityAssurance for GpMixture {
     }
 }
 
-#[typetag::serde]
+#[cfg_attr(feature = "serializable", typetag::serde)]
 impl MixtureGpSurrogate for GpMixture {
     /// Selected experts in the mixture
     fn experts(&self) -> &Vec<Box<dyn FullGpSurrogate>> {
